eee1472545ba5685549516720031b73a3e1dde7b
Pooja <newasepooja95@gmail.com>
2017-07-20 22:58:26 +0530

    After category dropdown

---
+++ src/app/component/advertisement/advertiseForm.ts
@@ -0,0 +1,12 @@
+import {Component, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+
+@Component({
+  selector: 'myadvertise',
+  templateUrl:'./advertiseform.html'
+})
+
+
+export class advertiseComponent  {
+  categories=[{cid:1,value:'furniture'},{cid:2,value:'Mobile'}]; 
+  }
